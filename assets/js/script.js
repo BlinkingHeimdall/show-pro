dbdd0a4fe2eeb86a6becc4b71961e133dfdeb3d0
Benny623 <bmilner88@gmail.com>
2022-03-03 13:33:49 -0700

    removed non-working js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,3 @@ $('#searchBtn').on('click', function(event){
     }
     console.log(input);
 });
-
-/* THIS DOES NOT WORK --
-$.ajax({
-    url: 'https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/top-tracks',
-    type: 'GET',
-    headers: {
-        'Authorization' : 'Bearer BQDO7RG85fTddaQgqaDc69pT-yEStgKcHBYg4wkf5tBZPt8-XOJMr2Gwi9AwVddckDEBLsZ7Qb3nmQxk4RhUDHDDW1XLL87H7jZ294v2kDt2-acP3aKA1Zrbeugg2dB_d2d37x5pZeqJcl3kXTL7ibdEsntDus-ieg',
-        'Content-Type' : 'application/json'
-    },
-    success: function(data) {
-        console.log(data);
-    }
-}); */
